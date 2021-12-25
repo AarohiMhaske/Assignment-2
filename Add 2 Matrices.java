ecf4dab41f74e38a8afc7d401a271a9405e92e2f
AarohiMhaske <96385997+AarohiMhaske@users.noreply.github.com>
2021-12-25 13:31:17 +0530

    Add files via upload

---
+++ Add 2 Matrices.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Addition of two matrices
+        nt a[][]={{1,5,8},{1,1,1},{6,6,6}};
+        int b[][]={{9,1,4},{2,2,2},{9,9,9}};
+        int c[][]=new int[3][3];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                c[i][j]=a[i][j]+b[i][j];
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
