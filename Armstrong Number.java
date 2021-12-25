cfbb075d37f0521b9ccae42cc95dac76e3af3571
AarohiMhaske <96385997+AarohiMhaske@users.noreply.github.com>
2021-12-25 13:33:13 +0530

    Add files via upload

---
+++ Armstrong Number.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Armstrong number
+        int number, temp, total = 0;
+
+        Scanner hm = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n =hm.nextInt();
+
+        number = n;
+        while (number != 0)
+        {
+            temp = number % 10;
+            total = total + temp*temp*temp;
+            number /= 10;
+        }
+
+        if(total == n)
+            System.out.println(n + " is an Armstrong number.");
+        else
+            System.out.println(n + " is not an Armstrong number.");
+    }
+}
