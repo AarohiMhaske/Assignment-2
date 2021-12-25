4253782b098b6cda8e9ec3ca7d37ca042fde391c
AarohiMhaske <96385997+AarohiMhaske@users.noreply.github.com>
2021-12-25 13:26:50 +0530

    Add files via upload

---
+++ Multiplication of 2 matrices.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Multiplication of two matrices
+        int m1[][] = {{1, 2, 3}, {4, 5, 6}, {2, 3, 4}};
+        int m2[][] = {{1, 2, 3}, {4, 5, 6}, {2, 3, 4}};
+        int m[][] = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    m[i][j] += m1[i][k] * m2[k][j];
+                }
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }}
